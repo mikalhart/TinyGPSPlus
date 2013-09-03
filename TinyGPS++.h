34d801ad8901d6c88af1aa5e49acd9d612b044ce
Mikal Hart <mhcom@sundial.com>
2013-09-03 00:33:17 -0500

    Initial commit

---
+++ TinyGPS++.h
@@ -0,0 +1,279 @@
+/*
+TinyGPS++ - a small GPS library for Arduino providing universal NMEA parsing
+Based on work by and "distance_to" and "course_to" courtesy of Maarten Lamers.
+Suggestion to add satellites(), course_to(), and cardinal(), by Matt Monson.
+Location recision improvements suggested by Wayne Holder.
+Copyright (C) 2008-2013 Mikal Hart
+All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef __TinyGPSPlus_h
+#define __TinyGPSPlus_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <limits.h>
+
+#define _GPS_VERSION 1 // software version of this library
+#define _GPS_MPH_PER_KNOT 1.15077945
+#define _GPS_MPS_PER_KNOT 0.51444444
+#define _GPS_KMPH_PER_KNOT 1.852
+#define _GPS_MILES_PER_METER 0.00062137112
+#define _GPS_KM_PER_METER 0.001
+#define _GPS_FEET_PER_METER 3.2808399
+#define _GPS_MAX_FIELD_SIZE 15
+/*
+   Field       Type    Format    Source                   Notes
+   -----       ----    ------    ------                   -----
+   Time        u32     hhmmsscc  GPRMC.1 OR GPGGA.1       Decimal
+   RmcValid    bool    isvalid   GPRMC.2                  =='A'
+   Latitude:   i32     dmill     GPRMC.3/4 OR GPGGA 2/3   Degrees (N/S)
+   Longitude   i32     dmill     GPRMC.5/6 or GPGGA.4/5   Degrees (E/W)
+   Speed       u32     kn100     GPRMC.7                  Decimal
+   Course      u32     deg100    GPRMC.8                  Decimal
+   Date        u32     ddmmyy    GPRMC.9                  
+   FixValid    bool    isgood    GPGGA.6                  > '0'
+   Satellites  u8      sats      GPGGA.7                  Decimal
+   HDOP        u32     hdop100   GPGGA.8                  Decimal
+   Altitude    i32     cm        GPGGA.9                  Decimal
+*/
+
+struct TinyGPSLocation
+{
+   friend class TinyGPSPlus;
+public:
+   bool isValid() const    { return valid; }
+   bool isUpdated() const  { return updated; }
+   uint32_t age() const    { return valid ? millis() - lastCommitTime : ULONG_MAX; }
+
+   int32_t rawLat()        { updated = false; return ilat; }
+   int32_t rawLng()        { updated = false; return ilng; }
+   double  lat();
+   double  lng();
+
+   TinyGPSLocation() : valid(false), updated(false), ilat(0), ilng(0)
+   {}
+
+private:
+   bool valid, updated;
+   int32_t ilat, newlat, ilng, newlng;
+   uint32_t lastCommitTime;
+   void commit();
+   void setLatitude(const char *term);
+   void setLongitude(const char *term);
+};
+
+struct TinyGPSDate
+{
+   friend class TinyGPSPlus;
+public:
+   bool isValid() const       { return valid; }
+   bool isUpdated() const     { return updated; }
+   uint32_t age() const       { return valid ? millis() - lastCommitTime : ULONG_MAX; }
+
+   uint32_t value()           { updated = false; return date; }
+   uint16_t year();
+   uint8_t month();
+   uint8_t day();
+
+   TinyGPSDate() : valid(false), updated(false), date(0)
+   {}
+
+private:
+   bool valid, updated;
+   uint32_t date, newDate;
+   uint32_t lastCommitTime;
+   void commit();
+   void setDate(const char *term);
+};
+
+struct TinyGPSTime
+{
+   friend class TinyGPSPlus;
+public:
+   bool isValid() const       { return valid; }
+   bool isUpdated() const     { return updated; }
+   uint32_t age() const       { return valid ? millis() - lastCommitTime : ULONG_MAX; }
+
+   uint32_t value()           { updated = false; return time; }
+   uint8_t hour();
+   uint8_t minute();
+   uint8_t second();
+   uint8_t centisecond();
+
+   TinyGPSTime() : valid(false), updated(false), time(0)
+   {}
+
+private:
+   bool valid, updated;
+   uint32_t time, newTime;
+   uint32_t lastCommitTime;
+   void commit();
+   void setTime(const char *term);
+};
+
+struct TinyGPSDecimal
+{
+   friend class TinyGPSPlus;
+public:
+   bool isValid() const    { return valid; }
+   bool isUpdated() const  { return updated; }
+   uint32_t age() const    { return valid ? millis() - lastCommitTime : ULONG_MAX; }
+   uint32_t value()        { updated = false; return val; }
+
+   TinyGPSDecimal() : valid(false), updated(false), val(0)
+   {}
+
+private:
+   bool valid, updated;
+   uint32_t lastCommitTime;
+   uint32_t val, newval;
+   void commit();
+   void set(const char *term);
+};
+
+struct TinyGPSInteger
+{
+   friend class TinyGPSPlus;
+public:
+   bool isValid() const    { return valid; }
+   bool isUpdated() const  { return updated; }
+   uint32_t age() const    { return valid ? millis() - lastCommitTime : ULONG_MAX; }
+   uint32_t value()        { updated = false; return val; }
+
+   TinyGPSInteger() : valid(false), updated(false), val(0)
+   {}
+
+private:
+   bool valid, updated;
+   uint32_t lastCommitTime;
+   uint32_t val, newval;
+   void commit();
+   void set(const char *term);
+};
+
+struct TinyGPSSpeed : TinyGPSDecimal
+{
+   double knots()    { return value() / 100.0; }
+   double mph()      { return _GPS_MPH_PER_KNOT * value() / 100.0; }
+   double mps()      { return _GPS_MPS_PER_KNOT * value() / 100.0; }
+   double kmph()     { return _GPS_KMPH_PER_KNOT * value() / 100.0; }
+};
+
+struct TinyGPSCourse : public TinyGPSDecimal
+{
+   double deg()      { return value() / 100.0; }
+};
+
+struct TinyGPSAltitude : TinyGPSDecimal
+{
+   double meters()       { return value() / 100.0; }
+   double miles()        { return _GPS_MILES_PER_METER * value() / 100.0; }
+   double kilometers()   { return _GPS_KM_PER_METER * value() / 100.0; }
+   double feet()         { return _GPS_FEET_PER_METER * value() / 100.0; }
+};
+
+class TinyGPSPlus;
+class TinyGPSCustom
+{
+public:
+   TinyGPSCustom() {};
+   TinyGPSCustom(TinyGPSPlus &gps, const char *sentenceName, int termNumber);
+   void begin(TinyGPSPlus &gps, const char *_sentenceName, int _termNumber);
+
+   bool isUpdated() const  { return updated; }
+   bool isValid() const    { return valid; }
+   uint32_t age() const    { return valid ? millis() - lastCommitTime : ULONG_MAX; }
+   const char *value()     { updated = false; return buffer; }
+
+private:
+   void commit();
+   void set(const char *term);
+
+   char stagingBuffer[_GPS_MAX_FIELD_SIZE + 1];
+   char buffer[_GPS_MAX_FIELD_SIZE + 1];
+   unsigned long lastCommitTime;
+   bool valid, updated;
+   const char *sentenceName;
+   int termNumber;
+   friend class TinyGPSPlus;
+   TinyGPSCustom *next;
+};
+
+class TinyGPSPlus
+{
+public:
+  TinyGPSPlus();
+  bool encode(char c); // process one character received from GPS
+  TinyGPSPlus &operator << (char c) {encode(c); return *this;}
+
+  TinyGPSLocation location;
+  TinyGPSDate date;
+  TinyGPSTime time;
+  TinyGPSSpeed speed;
+  TinyGPSCourse course;
+  TinyGPSAltitude altitude;
+  TinyGPSInteger satellites;
+  TinyGPSDecimal hdop;
+
+  static int libraryVersion() { return _GPS_VERSION; }
+
+  static double distanceBetween(double lat1, double long1, double lat2, double long2);
+  static double courseTo(double lat1, double long1, double lat2, double long2);
+  static const char *cardinal(double course);
+
+  static uint32_t parseDecimal(const char *term);
+  static uint32_t parseDegrees(const char *term);
+
+  uint32_t charsProcessed() const { return encodedCharCount; }
+  uint32_t goodSentences()  const { return goodSentenceCount; }
+  uint32_t failedChecksum() const { return failedChecksumCount; }
+  uint32_t passedChecksum() const { return passedChecksumCount; }
+
+private:
+  enum {GPS_SENTENCE_GPGGA, GPS_SENTENCE_GPRMC, GPS_SENTENCE_OTHER};
+
+  // parsing state variables
+  uint8_t parity;
+  bool isChecksumTerm;
+  char term[_GPS_MAX_FIELD_SIZE];
+  uint8_t curSentenceType;
+  uint8_t curTermNumber;
+  uint8_t curTermOffset;
+  bool gpsDataGood;
+
+  // custom element support
+  friend class TinyGPSCustom;
+  TinyGPSCustom *customElts;
+  TinyGPSCustom *customCandidate;
+  void insertCustom(TinyGPSCustom *pElt, const char *sentenceName, int index);
+
+  // statistics
+  uint32_t encodedCharCount;
+  uint32_t goodSentenceCount;
+  uint32_t failedChecksumCount;
+  uint32_t passedChecksumCount;
+
+  // internal utilities
+  int fromHex(char a);
+  bool endOfTermHandler();
+};
+
+#endif // def(__TinyGPSPlus_h)
---
+++ examples/UsingCustomFields/UsingCustomFields.ino
@@ -0,0 +1,81 @@
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+/*
+   This sample demonstrates TinyGPS++'s capacity for extracting custom
+   fields from any NMEA sentence.  TinyGPS++ has built-in facilities for
+   extracting latitude, longitude, altitude, etc., from the $GPGLL and 
+   $GPRMC sentences.  But with the TinyGPSCustom type, you can extract
+   other NMEA fields, even from non-standard NMEA sentences.
+
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(RX) and 3(TX).
+*/
+static const int RXPin = 4, TXPin = 3;
+static const int GPSBaud = 4800;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+/*
+   By declaring TinyGPSCustom objects like this, we announce that we
+   are interested in the 15th, 16th, and 17th fields in the $GPGSA 
+   sentence, respectively the PDOP (F("positional dilution of precision")),
+   HDOP (F("horizontal...")), and VDOP (F("vertical...")).
+
+   (Counting starts with the field immediately following the sentence name, 
+   i.e. $GPGSA.  For more information on NMEA sentences, consult your
+   GPS module's documentation and/or http://aprs.gids.nl/nmea/.)
+
+   If your GPS module doesn't support the $GPGSA sentence, then you 
+   won't get any output from this program.
+*/
+
+TinyGPSCustom pdop(gps, "GPGSA", 15); // $GPGSA sentence, 15th element
+TinyGPSCustom hdop(gps, "GPGSA", 16); // $GPGSA sentence, 16th element
+TinyGPSCustom vdop(gps, "GPGSA", 17); // $GPGSA sentence, 17th element
+
+void setup() 
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+
+  Serial.println(F("UsingCustomFields.ino"));
+  Serial.println(F("Demonstrating how to extract any NMEA field using TinyGPSCustom"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop() 
+{
+  // Every time anything is updated, print everything.
+  if (gps.altitude.isUpdated() || gps.satellites.isUpdated() ||
+    pdop.isUpdated() || hdop.isUpdated() || vdop.isUpdated())
+  {
+    Serial.print(F("ALT="));   Serial.print(gps.altitude.meters()); 
+    Serial.print(F(" PDOP=")); Serial.print(pdop.value()); 
+    Serial.print(F(" HDOP=")); Serial.print(hdop.value()); 
+    Serial.print(F(" VDOP=")); Serial.print(vdop.value());
+    Serial.print(F(" SATS=")); Serial.println(gps.satellites.value());
+  }
+  smartDelay(1000);
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+}
+
+// This custom version of delay() ensures that the gps object
+// is being "fed".
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
