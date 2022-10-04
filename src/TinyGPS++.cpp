e8e2be5bbf06d6cbe2ae25d895778fee6cd5f807
Aldric-shield <aldric@shield-robotics.com>
2022-10-04 11:54:28 +0200

    Added GLRMC and GLGGA support

--- src/TinyGPS++.cpp
+++ src/TinyGPS++.cpp
@@ -31,6 +31,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define _GPGGAterm   "GPGGA"
 #define _GNRMCterm   "GNRMC"
 #define _GNGGAterm   "GNGGA"
+#define _GLRMCterm   "GLRMC"
+#define _GLGGAterm   "GLGGA"
 
 TinyGPSPlus::TinyGPSPlus()
   :  parity(0)
@@ -209,9 +211,9 @@ bool TinyGPSPlus::endOfTermHandler()
   // the first term determines the sentence type
   if (curTermNumber == 0)
   {
-    if (!strcmp(term, _GPRMCterm) || !strcmp(term, _GNRMCterm))
+    if (!strcmp(term, _GPRMCterm) || !strcmp(term, _GNRMCterm) || !strcmp(term, _GLRMCterm))
       curSentenceType = GPS_SENTENCE_GPRMC;
-    else if (!strcmp(term, _GPGGAterm) || !strcmp(term, _GNGGAterm))
+    else if (!strcmp(term, _GPGGAterm) || !strcmp(term, _GNGGAterm) || !strcmp(term, _GLGGAterm))
       curSentenceType = GPS_SENTENCE_GPGGA;
     else
       curSentenceType = GPS_SENTENCE_OTHER;
